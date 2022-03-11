6d39d50df6d1a4e056f9ae586c8e016a2f27d9df
silasevan <silasevan@gmail.com>
2022-03-11 07:15:01 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main -Entry point
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"programming is like building a multilinqual puzzle");
+	puts("\"programming is like building a multilinqual puzzle\n");
 	return (0);
 }
