2813df09a78043db3af53c3cbf393da163bd4759
silasevan <silasevan@gmail.com>
2022-03-12 15:18:44 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+/*
+ * file - 3-print_alphabets.c
+ */
+
+#include <stdio.h>
+
+
+/**
+ * main - print upper and lower case
+ *
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A';letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
