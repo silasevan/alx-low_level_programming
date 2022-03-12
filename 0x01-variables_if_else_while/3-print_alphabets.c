accc9465d94dc69226fb7ddf30fcc0393bcf22c4
silasevan <silasevan@gmail.com>
2022-03-12 15:27:10 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,14 +12,13 @@
  */
 
 int main(void)
-
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	for (letter = 'A';letter <= 'Z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
