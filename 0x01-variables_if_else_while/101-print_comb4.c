9a907c45dbc1deadfd265e0507abc03a36184e3f
silasevan <silasevan@gmail.com>
2022-03-13 06:42:56 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+/*
+ * file - 101-print_comb4.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print combo
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 8; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 9; num2++)
+		{
+			for (num3 = num2 + 1; num3 < 10; num3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
