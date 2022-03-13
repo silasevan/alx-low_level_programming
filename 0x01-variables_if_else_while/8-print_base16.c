8beb23679e96c07b9d972a10f7919475aa86ac8f
silasevan <silasevan@gmail.com>
2022-03-12 17:27:16 -0800

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -16,7 +16,7 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num <= 9; num++)
 		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
