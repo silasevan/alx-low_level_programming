f6c7cb9ef7be81e51f5d8d015441bb2fc0bab7b0
silasevan <silasevan@gmail.com>
2022-03-13 08:05:00 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+/*
+ * file - 101--quote.c
+ */
+
+#include <stdio.c>
+
+/**
+ * main - print "and that piece of art is useful" -Dora Korper, 2015-10-19",
+ * 		follow by a new line, to stardard error.
+ *
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+
+	write(2,
+	      "and that piece of art is useful\" - Dora Korper, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
