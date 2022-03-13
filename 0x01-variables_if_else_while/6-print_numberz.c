65a54452313afcc3fb11d9acd9323ad92cfdd48c
silasevan <silasevan@gmail.com>
2022-03-12 16:22:06 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/*
+ * file - 6-print_numberz
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print all base 10 number
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 0; num < 9; num++)
+		putchar("%d" , num);
+
+	putchar("\n");
+
+	return (0);
+}
