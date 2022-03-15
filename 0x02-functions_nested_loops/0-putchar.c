102fe115129c07c6caa05896034b9c875146148d
silasevan <silasevan@gmail.com>
2022-03-15 14:48:02 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+/*
+ * file - 0-putchar.c
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -To print putchar
+ *
+ * Return: always 0
+ */
+
+int main()
+{
+
+	putchar(_putchar());
+
+	putchar('\n');
+
+	return (0);
+}
+
+int _putchar(char c)
+{
+
+	return (write(1, &c, 1));
+}
+
+
