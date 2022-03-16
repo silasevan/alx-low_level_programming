cd347571f1d4543af504c60442386be60b8582b3
silasevan <silasevan@gmail.com>
2022-03-16 02:34:15 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,7 +6,7 @@
 
 /**
  * _abs - Computes the  absolute value of an integer.
- * @n: The  integer to be computed
+ * @n: The integer to be computed
  * 
  * Return: The absolute value of the integer.
  */
