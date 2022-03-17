c498b62a9f3ca4ee3ebec355824e502395056cf1
silasevan <silasevan@gmail.com>
2022-03-17 10:17:57 -0700

    supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+/*
+ * file - 0-isupper.c
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  main - check the code
+ *
+ *  Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+/*
+ * file - 0-isupper.c
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+
+#endif
