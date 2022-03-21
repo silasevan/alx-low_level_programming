3b8c0be26ac1e37412299cb283653fef7b591cba
silasevan <silasevan@gmail.com>
2022-03-21 14:24:02 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,37 @@
 /**
  * print_rev - print string in reverse.
  * @s: string to be printed.
+ *
+ * Return Always 0.
  */
 
 void print_rev(char *s)
 {
 
-	int len = 0, index;
+	/*int len = 0, index;
 
 	while (s[index++])
 		len++;
 
-	for (index = len -1; index >= 0; index--)
+	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
 
-	_putchar(10);
+	_putchar(10);*/
+
+	char*str;
+	int len;
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+
+	return (0);
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		for (len = len - 1; len >= 0; len--)
+		{
+			_putchar(s[len]);
+		}
+
+		_putchar(10);
+	}
 }
