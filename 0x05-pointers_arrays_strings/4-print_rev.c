723d3cf981561e2d6b8c509ee69c82212908eb55
silasevan <silasevan@gmail.com>
2022-03-21 14:27:50 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,7 +14,7 @@
 void print_rev(char *s)
 {
 
-	/*int len = 0, index;
+	int len = 0, index;
 
 	while (s[index++])
 		len++;
@@ -22,22 +22,5 @@ void print_rev(char *s)
 	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
 
-	_putchar(10);*/
-
-	char*str;
-	int len;
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-
-	return (0);
-
-	for (len = 0; s[len] != '\0'; len++)
-	{
-		for (len = len - 1; len >= 0; len--)
-		{
-			_putchar(s[len]);
-		}
-
-		_putchar(10);
-	}
+	_putchar(10);
 }
