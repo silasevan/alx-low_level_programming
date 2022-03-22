f7d73c94257f0c26657dfbb90ea9535bb4dd8968
silasevan <silasevan@gmail.com>
2022-03-21 18:54:41 -0700

    2 srting

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+/*
+ * File: 6-puts2.c
+ */
+
+#include "main.h"
+
+/**
+ * puts2 - Prints one char out of two of a string.
+ * @str: The string containing characters.
+ */
+
+void puts2(char *str)
+
+{
+
+	while (*str)
+
+	{
+
+		_putchar(*str);
+
+		str += 2;
+
+	}
+
+
+
+	_putchar('\n');
+
+}
+
