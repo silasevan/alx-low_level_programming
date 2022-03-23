561ba21eaa478d63799f33225cba02318eaafcf4
silasevan <silasevan@gmail.com>
2022-03-23 14:57:04 -0700

    mmm

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -33,7 +33,7 @@ int _strcmp(char *s1, char *s2)
 
 {
 
-	while (*s1 && *s2 *s1 == *s2)
+	while (*s1 && *s1 == *s2)
 
 	{
 
@@ -43,7 +43,7 @@ int _strcmp(char *s1, char *s2)
 
 	}
 
-	return (*s1 - *s2);
+	 return (int)(unsigned char)(*s1) - (int)(unsigned char)(*s2);
 
 
 }
