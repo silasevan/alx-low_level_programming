f2323c2bffe178db398eaa41066721a97a59280d
silasevan <silasevan@gmail.com>
2022-04-01 17:52:45 -0700

    shh

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,30 @@
+/*
+ * file - 0-putchar.c
+ */
+
+
+#include "main.h"
+
+/**
+ * main -To print putchar
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
+
