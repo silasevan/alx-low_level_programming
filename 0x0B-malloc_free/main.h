8ae16230aaf0fafd07d539833df900781602fb2c
silasevan <silasevan@gmail.com>
2022-04-04 18:23:41 -0700

    free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,14 +1,20 @@
-#ifdef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_HEADER
+#define _MAIN_HEADER
+
+int _putchar(char c);
 
 char *create_array(unsigned int size, char c);
+
 char *_strdup(char *str);
-int **alloc_grid(int width, int height);
+
 char *str_concat(char *s1, char *s2);
 
+int **alloc_grid(int width, int height);
 
+void free_grid(int **grid, int height);
 
+char *argstostr(int ac, char **av);
 
-
+char **strtow(char *str);
 
 #endif
