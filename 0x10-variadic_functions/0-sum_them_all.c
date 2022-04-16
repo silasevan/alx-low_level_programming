83924121d06071d5907573d631eb0f787bba7150
silasevan <silasevan@gmail.com>
2022-04-15 21:51:31 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,21 +17,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	unsigned int index, sum = 0;
 
-
-
 	va_start(nums, n);
-
-
 
 	for (index = 0; index < n; index++)
 
 		sum += va_arg(nums, int);
 
-
-
 	va_end(nums);
-
-
 
 	return (sum);
 }
