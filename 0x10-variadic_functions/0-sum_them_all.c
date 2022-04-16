5440d21e3a8246c51392c4545b6f08600ed0c76f
silasevan <silasevan@gmail.com>
2022-04-15 21:42:51 -0700

    header file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,27 +13,17 @@ int sum_them_all(const unsigned int n, ...)
 
 	unsigned int sum = 0, i;
 
-
-
 	if (n == 0)
 
 		return (0);
 
-
-
 	va_start(valist, n);
-
-
 
 	for (i = 0; i < n; i++)
 
 		sum += va_arg(valist, int);
 
-
-
 	va_end(valist);
-
-
 
 	return (sum);
 }
