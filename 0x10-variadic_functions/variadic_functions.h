d63c2c5838d2c0bd58d466b682a4494cd6109f3b
silasevan <silasevan@gmail.com>
2022-04-15 21:06:46 -0700

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -17,12 +17,9 @@ typedef struct check
 	void (*f)(va_list list);
 } check_t;
 
-
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif
